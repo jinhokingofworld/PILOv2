930438b1238c4abde112085dc18281042b2426cf
Sein0104 <kimsein0104@gmail.com>
2026-07-21 17:59:17 +0900

    refactor: Pretendard 공통 폰트 적용 (#1674)

--- apps/frontend/src/features/home/page.tsx
+++ apps/frontend/src/features/home/page.tsx
@@ -1,18 +1,5 @@
-import localFont from "next/font/local";
-
 import { HomeDashboard } from "@/features/home/components/home-dashboard";
 
-const pretendard = localFont({
-  display: "swap",
-  src: "./assets/fonts/PretendardVariable.woff2",
-  variable: "--font-pretendard",
-  weight: "45 920"
-});
-
 export function HomePage() {
-  return (
-    <div className={`${pretendard.className} flex min-h-0 flex-1 flex-col`}>
-      <HomeDashboard />
-    </div>
-  );
+  return <HomeDashboard />;
 }
